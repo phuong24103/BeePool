298250aceffa70d68de4a7386a505e3e620a21ea
Phuong Pham <phamphuong241003@gmail.com>
2023-10-24 23:11:43 +0700

    admin

---
+++ Datn_Client/Areas/Admin/Controllers/ShaftController.cs
@@ -0,0 +1,61 @@
+﻿using Datn_Shared.Models;
+using Datn_Shared.ViewModels.ShaftViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Datn_Client.Areas.Admin.Controllers
+{
+    public class ShaftController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        public ShaftController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                var categories = await _httpClient.GetFromJsonAsync<IEnumerable<Shaft>>("\"https://localhost:7033/api/Shaft/GetAll");
+                return View(categories);
+            }
+            else
+            {
+                var categories = await _httpClient.GetFromJsonAsync<IEnumerable<Shaft>>("\"https://localhost:7033/api/Shaft/GetAll");
+                var category = await _httpClient.GetFromJsonAsync<Shaft>($"\"https://localhost:7033/api/Shaft/GetById/{id}");
+                List<Shaft> c = new List<Shaft>();
+                c.Add(category);
+                ViewData["s"] = c;
+                return View(categories);
+            }
+        }
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm] CreateShaft createShaft)
+        {
+            await _httpClient.PostAsJsonAsync($"https://localhost:7033/api/Shaft/Create", createShaft);
+            return RedirectToAction("Index", "Tip");
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _httpClient.DeleteAsync($"https://localhost:7033/api/Shaft/Delete/{id}");
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public async Task<IActionResult> Update(Guid id)
+        {
+            var t = await _httpClient.GetFromJsonAsync<Tip>($"https://localhost:7033/api/Shaft/GetById/{id}");
+            return View(t);
+        }
+
+        public async Task<IActionResult> Update(Guid id, UpdateShaft updateShaft)
+        {
+            await _httpClient.PutAsJsonAsync($"https://localhost:7033/api/Shaft/Update/{id}", updateShaft);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Datn_Client/Areas/Admin/Controllers/WeightController.cs
@@ -0,0 +1,62 @@
+﻿using Datn_Shared.Models;
+using Datn_Shared.ViewModels.WeightViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Datn_Client.Areas.Admin.Controllers
+{
+    public class WeightController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        public WeightController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                var categories = await _httpClient.GetFromJsonAsync<IEnumerable<Weight>>("https://localhost:7033/api/Weight/GetAll");
+                return View(categories);
+            }
+            else
+            {
+                var categories = await _httpClient.GetFromJsonAsync<IEnumerable<Weight>>("https://localhost:7033/api/Weight/GetAll");
+                var category = await _httpClient.GetFromJsonAsync<Weight>($"https://localhost:7033/api/Weight/GetById/{id}");
+                List<Weight> c = new List<Weight>();
+                c.Add(category);
+                ViewData["w"] = c;
+                return View(categories);
+            }
+        }
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromForm] CreateWeight createWeight)
+        {
+            await _httpClient.PostAsJsonAsync($"https://localhost:7033/api/Weight/Create", createWeight);
+            return RedirectToAction("Index", "Weight");
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _httpClient.DeleteAsync($"https://localhost:7033/api/Weight/Delete/{id}");
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public async Task<IActionResult> Update(Guid id)
+        {
+            var t = await _httpClient.GetFromJsonAsync<Weight>($"https://localhost:7033/api/Weight/GetById/{id}");
+            return View(t);
+        }
+
+        public async Task<IActionResult> Update(Guid id, UpdateWeight updateWeight)
+        {
+            await _httpClient.PutAsJsonAsync($"https://localhost:7033/api/Weight/Update/{id}", updateWeight);
+            return RedirectToAction("Index");
+        }
+    }
+}
